6f6660545d2c0f9cc98760347fd987fddac33eed
Vincent Kiathadi <vincentkiathadi@gmail.com>
2023-09-05 22:29:50 +0800

    javascript

--- script.js
+++ script.js
@@ -2,34 +2,36 @@ var cafez=[
     {name:"Cafe Rum",
     Location: "Carribean",
     Phone:"12345678",
-    Image:"./image/cafe1.jpg"
+    Image:"image/cafe1.jpg"
     },
     {name:"Cafe Licia",
     Location: "French",
     Phone:"7911131517",
-    Image:"./image/cafe2.jpg"
+    Image:"image/cafe2.jpg"
     },
     {name:"Cafe Tuccini",
     Location: "Italy",
     Phone:"369121518",
-    Image:"./image/cafe3.jpg"
+    Image:"image/cafe3.jpg"
     },
     {name:"Cafe Ver",
     Location: "China",
     Phone:"930301003",
-    Image:"./image/cafe3.jpg"
+    Image:"./image/cafe4.jpg"
     }
 ];
 
-console.log("A");
+console.log(cafez[2].Location);
+
+var x="elon";
 
 
 cafez.forEach(cafeFunction=>{
     let cafecyril=document.createElement("button");
     cafecyril.phone=cafez.phone;
-    cafecyril.innerHTML=`<img src="${cafez.Image}"/><br/>
-                        <span>Cafe Name: ${cafez.name}</span><br/>
-                        <span>Cafe Location: ${cafez.Location}</span><br/>`;
+    cafecyril.innerHTML=`<img src=${cafeFunction.Image} class="cafeImage"><br/>
+                        <span>Cafe Name: ${cafeFunction.name}</span><br/>
+                        <span>Cafe Location: ${cafeFunction.Location}</span><br/>`;
 
     document.getElementById("showCafe").appendChild(cafecyril);
 });
@@ -41,7 +43,7 @@ cafez.forEach(cafeFunction=>{
 
 // function showCafeFunction(){
 //     let newCafe=document.createElement("button");
-//     newCafe.innerHTML=`<img src="${cafez.Image}"/><br/>
+//     newCafe.innerHTML=`<img src=${cafez.Image}><br/>
 //                         Cafe Name: ${cafez.name}<br/>
 //                         Cafe Location: ${cafez.Location}<br/>`;
 
